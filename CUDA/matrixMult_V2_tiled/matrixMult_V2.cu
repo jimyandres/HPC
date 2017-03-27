ca1a22726d1dea6dc44a13bd45d16e17f3dcdc56
jimyandres <jimyandres@utp.edu.co>
2017-03-27 13:11:05 -0500

    New improvements in codification, use of flags to execute the program.

--- CUDA/matrixMult_V2_tiled/matrixMult_V2.cu
+++ CUDA/matrixMult_V2_tiled/matrixMult_V2.cu
@@ -45,47 +45,166 @@ __global__ void matrixMulKernelTiled(double *A, double *B, double *C, int N){
 __global__ void matrixMultGPU (double *A, double *B, double *C, int N){
 	int col = threadIdx.x + blockDim.x * blockIdx.x;
 	int row = threadIdx.y + blockDim.y * blockIdx.y;
-	double acc;
+	double ans;
 	if(col < N && row < N){
-		acc = 0.0;
+		ans = 0.0;
 		for(int k=0;k<N;k++)
-			acc += A[row*N+k] * B[k*N+col];
-		C[row*N+col] = acc;
+			ans += A[row*N+k] * B[k*N+col];
+		C[row*N+col] = ans;
 	}
 }
 
 void matrixMultCPU(double *A, double *B, double *C, int N){
-	double acc;
+	double ans;
 	for(int i=0;i<N;i++){
 		for(int j=0;j<N;j++){
-			acc=0.0;
+			ans=0.0;
 			for(int k=0;k<N;k++)
-				acc += A[i*N+k]*B[k*N+j];
-			C[i*N+j] = acc;
+				ans += A[i*N+k]*B[k*N+j];
+			C[i*N+j] = ans;
 		}
 	}
 }
 
 std::string testValues(double *A, double *B, int N){
-    for(int i = 0; i < N; ++i)
-        for(int j = 0; j < N; ++j)
-            if(A[(i*N)+j]!=B[(i*N)+j]){
-                return "Mal Cálculo";
-            }
-    return "Buen Cálculo";
+	for(int i = 0; i < N; ++i)
+		for(int j = 0; j < N; ++j)
+			if(A[(i*N)+j]!=B[(i*N)+j]){
+				return "Mal Cálculo";
+			}
+		return "Buen Cálculo";
+}
+
+void printMatrix(double *A, int N){
+	for(int i=0;i<N*N;i++){
+		if(i%N == 0)
+		printf("\n");
+			printf("%f; ",A[i]);
+	}
+	printf("\n---------\n");
 }
 
 
-int main(int argc, char **argv){
-	cudaError_t error = cudaSuccess;	
-	double *A, *B, *C1, *C2;
+void serial(double *A, double *B, double *C, double &time, int N) {
+
+	/*******************************HOST********************************/
+	clock_t tic = clock();
+	matrixMultCPU(A,B,C, N);
+  	clock_t toc = clock();
+	time = (double)(toc - tic) / CLOCKS_PER_SEC;
+	/*****************************END HOST******************************/
+
+}
+
+void checkError(cudaError_t error, std::string type) {
+	if(error != cudaSuccess){
+		printf("Error in %s\n", type.c_str());
+		exit(0);
+	}
+}
+
+void cuda(double *A, double *B, double *C, double &time, double size, int N) {
+	cudaError_t error = cudaSuccess;
 	double *d_A, *d_B, *d_C;
-	double CPU, GPU_tiled;
-	if(argc != 2) {
+
+	error = cudaMalloc((void**)&d_A,size);
+	checkError(error, "cudaMalloc for d_A (cuda)");
+
+	error = cudaMalloc((void**)&d_B,size);
+	checkError(error, "cudaMalloc for d_B (cuda)");
+
+	error = cudaMalloc((void**)&d_C,size);
+	checkError(error, "cudaMalloc for d_C (cuda)");
+
+	/*******************************GPU********************************/
+	clock_t tic = clock();
+
+	error = cudaMemcpy(d_A,A,size,cudaMemcpyHostToDevice);
+	checkError(error, "cudaMemcpy for d_A (cuda)");
+
+	error = cudaMemcpy(d_B,B,size,cudaMemcpyHostToDevice);
+	checkError(error, "cudaMemcpy for d_B (cuda)");
+		
+	dim3 dimBlock(32,32);
+	dim3 dimGrid(ceil(N/float(dimBlock.x)),ceil(N/float(dimBlock.y)));
+
+	matrixMultGPU<<<dimGrid,dimBlock>>>(d_A,d_B,d_C,N);
+	cudaDeviceSynchronize();
+	error = cudaMemcpy(C,d_C,size,cudaMemcpyDeviceToHost);
+	checkError(error, "cudaMemcpy for C (cuda)");
+
+	clock_t toc = clock();
+	time = (double)(toc - tic) / CLOCKS_PER_SEC;
+	/*****************************GPU END******************************/
+
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
+}
+
+void cuda_tiled(double *A, double *B, double *C, double &time, double size, int N) {
+	cudaError_t error = cudaSuccess;
+	double *d_A, *d_B, *d_C;
+
+	error = cudaMalloc((void**)&d_A,size);
+	checkError(error, "cudaMalloc for d_A (cuda with tiling)");
+
+	error = cudaMalloc((void**)&d_B,size);
+	checkError(error, "cudaMalloc for d_B (cuda with tiling)");
+
+	error = cudaMalloc((void**)&d_C,size);
+	checkError(error, "cudaMalloc for d_C (cuda with tiling)");
+
+	/*******************************GPU TILED********************************/
+	clock_t tic = clock();
+
+	error = cudaMemcpy(d_A,A,size,cudaMemcpyHostToDevice);
+	checkError(error, "cudaMemcpy for d_A (cuda with tiling)");
+
+	error = cudaMemcpy(d_B,B,size,cudaMemcpyHostToDevice);
+	checkError(error, "cudaMemcpy for d_B (cuda with tiling)");
+	
+	dim3 dimBlock(TILE_WIDTH,TILE_WIDTH);
+	dim3 dimGrid(ceil(N/float(dimBlock.x)),ceil(N/float(dimBlock.y)));
+
+	matrixMulKernelTiled<<<dimGrid,dimBlock>>>(d_A,d_B,d_C,N);
+	cudaDeviceSynchronize();
+	error = cudaMemcpy(C,d_C,size,cudaMemcpyDeviceToHost);
+	checkError(error, "cudaMemcpy for C (cuda with tiling)");
+
+	clock_t toc = clock();
+	time = (double)(toc - tic) / CLOCKS_PER_SEC;
+	/*****************************GPU TILED END******************************/
+
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
+}
+
+
+int main(int argc, char **argv){	
+	double *A, *B, *C1, *C2, *C3;
+	double CPU, GPU, GPU_tiled, acc1, acc2;
+	CPU = GPU = GPU_tiled = acc1 = acc2 = 0.0;
+	
+	// Meaning of  positions: {serial, cuda, cuda+tiling}
+	bool op[] = {false, false, false};
+
+	if(argc < 2) {
 		printf("No size given\n");
 		return -1;
 	}
 	int N = atoi(argv[1]);
+
+	for (int i = 2; i < argc; i++) {
+		std::string s = argv[i];
+		if (s == "s")
+			op[0] = true;
+		else if (s == "c")
+			op[1] = true;
+		else if (s == "ct")
+			op[2] = true;
+	}
 
 	double size = N*N*sizeof(double);
 
@@ -93,108 +212,52 @@ int main(int argc, char **argv){
  	B = (double*)malloc(size);
  	C1 = (double*)malloc(size);
  	C2 = (double*)malloc(size);
-
-	if (C2 == NULL)
-		return 0;
+	C3 = (double*)malloc(size);
 
 	for(int i=0;i<N*N;i++){
 			A[i]=1;
 			B[i]=2;
 	}
 
-	/*******************************HOST********************************/
-	clock_t tic = clock();
-	matrixMultCPU(A,B,C1, N);
-  	clock_t toc = clock();
-	//printf("Tiempo CPU: %f segundos", (double)(toc - tic) / CLOCKS_PER_SEC);
-	CPU = (double)(toc - tic) / CLOCKS_PER_SEC;
-	printf("%f,",CPU);
-	/*****************************END HOST******************************/
-  
-	error = cudaMalloc((void**)&d_A,size);
-	if(error != cudaSuccess){
-		printf("Error in cudaMalloc for d_A\n");
-		exit(0);
+	if (op[0]) serial(A, B, C1, CPU, N);
+	if (op[1]) cuda(A, B, C2, GPU, size, N);
+	if (op[2]) cuda_tiled(A, B, C3, GPU_tiled, size, N);
+
+	if (op[0]) {
+		printf(" %f |", CPU);
 	}
-	
-	error = cudaMalloc((void**)&d_B,size);
-	if(error != cudaSuccess){
-                printf("Error in cudaMalloc for d_B\n");
-                exit(0);
-        }
+	else printf(" - |");
 
-	error = cudaMalloc((void**)&d_C,size);
-	if(error != cudaSuccess){
-                printf("Error in cudaMalloc for d_C\n");
-                exit(0);
-        }
-
-	error = cudaMemcpy(d_A,A,size,cudaMemcpyHostToDevice);
-	if(error != cudaSuccess){
-                printf("Error in cudaMemcpy for d_A\n");
-                exit(0);
-        }
-
-	error = cudaMemcpy(d_B,B,size,cudaMemcpyHostToDevice);
-	if(error != cudaSuccess){
-                printf("Error in cudaMemcpy for d_B\n");
-                exit(0);
-        }
-
-	/*******************************GPU TILED********************************/
-	dim3 dimBlock(TILE_WIDTH, TILE_WIDTH,1);
-	dim3 dimGrid(ceil(N/float(dimBlock.x)),ceil(N/float(dimBlock.y)),1);
-	
-  	clock_t tic2 = clock();
-	matrixMulKernelTiled<<<dimGrid,dimBlock>>>(d_A,d_B,d_C,N);
-	//matrixMultGPU<<<dimGrid,dimBlock>>>(d_A,d_B,d_C,N);
-
-	cudaDeviceSynchronize();
-
-	cudaMemcpy(C2,d_C,size,cudaMemcpyDeviceToHost);
-  	clock_t toc2 = clock();
-	//printf("\n\nTiempo GPU: %f segundos\n", (double)(toc2 - tic2) / CLOCKS_PER_SEC);
-	GPU_tiled = (double)(toc2 - tic2) / CLOCKS_PER_SEC;
-	printf("%f,%f,%s\n", GPU_tiled, (CPU/GPU_tiled), testValues(C1,C2,N).c_str());
-	/*****************************GPU TILED END******************************/
-  	/*
-  	for(int i=0;i<N*N;i++){
-		if(i%N == 0)
-		printf("\n");
-			printf("%f ;",A[i]);
+	if (op[1]) {
+		if (op[0]) {
+			acc1 = CPU / GPU;
+			std::string r1 = testValues(C1, C2, N);
+			printf(" %f | %f | %s |", GPU, acc1, r1.c_str());
+		}
+		else printf(" %f | - | - |", GPU);
 	}
-	printf("\n---------\n");
-	
-	for(int i=0;i<N*N;i++){
-		if(i%N == 0)
-		printf("\n");
-			printf("%f ;",B[i]);
-	}
-	printf("\n---------\n");
-	for(int i=0;i<N*N;i++){
-		if(i%N == 0)
-		printf("\n");
-			printf("%f ;",C1[i]);
-	}
-	printf("\n---------\n");
-	for(int i=0;i<N*N;i++){
-                if(i%N == 0)
-                printf("\n");
-                        printf("%f ;",C2[i]);
-        }
-        printf("\n---------\n");
-	*/	
+	else printf(" - | - | - |");
 
+	if (op[2]) {
+		if (op[1]) {
+			acc2 = GPU / GPU_tiled;
+			std::string r1 = testValues(C2, C3, N);
+			printf(" %f | %f | %s |\n", GPU_tiled, acc2, r1.c_str());
+		}
+		else if (op[0]) {
+			acc1 = CPU / GPU_tiled;
+			std::string r1 = testValues(C1, C3, N);
+			printf(" %f | %f | %s |\n", GPU_tiled, acc1, r1.c_str());
+		}
+		else printf(" %f | - | - |\n", GPU_tiled);
+	}
+	else printf(" - | - | - |\n");
 
 	free(A);
 	free(B);
 	free(C1);
 	free(C2);
-
-	cudaFree(d_A);
-	cudaFree(d_B);
-	cudaFree(d_C);
-
+	free(C3);
 	
 	return 0;
 }
--- CUDA/matrixMult_V2_tiled/matrixMult_V2.sh
+++ CUDA/matrixMult_V2_tiled/matrixMult_V2.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 #
 #SBATCH --job-name=matrixMult_V2
-#SBATCH --output=res_matrixMult_V2.csv
+#SBATCH --output=res_matrixMult_V2.md
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --time=10:00
 #SBATCH --gres=gpu:1
 
 for i in {100,500,1000,1500}; do
-	echo "N = $i",,,,
+	echo "**N** = $i"
+	echo
+	echo "| n | Serial | CUDA w/o CheckMem | Acceleration | Check Resutl | CUDA w/ CheckMem | Acceleration | Check Resutl |"
+	echo "| :---: | :---: | :---: | :---: | :---: | :---: | :---: | :---: |"
 	for (( j = 0; j < 20; j++ )); do
-		echo -n "$j,"
-		srun matrixMult_V2 $i 
+		echo -n "| $j | "
+		srun matrixMult_V2 $i s c ct 
 	done
+	echo
 done
