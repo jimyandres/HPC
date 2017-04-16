d8f166551ed72da8ea804ece33766b07c6b54abf
jimyandres <jimyandres@utp.edu.co>
2017-04-16 10:41:10 -0500

    New results

--- CUDA/ImageConvolution/ImageConvolution.sh
+++ CUDA/ImageConvolution/ImageConvolution.sh
@@ -8,14 +8,17 @@
 #SBATCH --gres=gpu:1
 
 
-i="test1.png"
+declare -a array=("test2.png" "test1.png" "test3.jpg")
 
-echo "**Image** = $i"
-echo
-echo "| n | Sequential on Host | Sobel on Host | Acceleration | Sequential on Device | Acceleration | Sobel on Device | Acceleration |"
-echo "| :---: | :---: | :---: | :---: | :---: | :---: | :---: | :---: |"
-for (( j = 1; j <= 20; j++ )); do
-	echo -n "| $j | "
-	srun ImageConvolution $i seq_h sobel_h seq_d sobel_d 
+for i in "${array[@]}"
+do
+	echo "**Image** = $i"
+	echo
+	echo "| n | Sequential on Host | Sobel on Host | Acceleration | Sequential on Device | Acceleration | Sobel on Device | Acceleration |"
+	echo "| :---: | :---: | :---: | :---: | :---: | :---: | :---: | :---: |"
+	for (( j = 1; j <= 20; j++ )); do
+		echo -n "| $j | "
+		srun ImageConvolution $i seq_h sobel_h seq_d sobel_d 
+	done
+	echo
 done
-echo
